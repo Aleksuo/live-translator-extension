9823296dfb7135d9d42e82dd5454fd0c30f12443
Aleksi Suoranta <aleksi.suoranta@gmail.com>
2025-02-16 20:38:35 +0200

    Remove old comment

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,9 @@
 document.getElementById("openOptionsBtn").addEventListener("click", () => {
-    const url = chrome.runtime.getURL("options_ui/page.html");
-    // Store that tab ID somehow, maybe in chrome.storage or pass it as a URL param
-    // Then open your options page in a new window or tab
-    chrome.windows.create({
-      url: url,
-      type: "popup",
-      width: 400,
-      height: 500,
-    });
+  const url = chrome.runtime.getURL("options_ui/page.html");
+  chrome.windows.create({
+    url: url,
+    type: "popup",
+    width: 400,
+    height: 500,
+  });
 });
