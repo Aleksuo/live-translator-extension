0d9165e5597168eab5da1446de94e4da8a89e0e1
Aleksi Suoranta <aleksi.suoranta@gmail.com>
2025-02-16 21:05:55 +0200

    Format project using biome

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,9 +1,9 @@
 document.getElementById("openOptionsBtn").addEventListener("click", () => {
-	const url = chrome.runtime.getURL("options_ui/page.html");
-	chrome.windows.create({
-		url: url,
-		type: "popup",
-		width: 400,
-		height: 500,
-	});
+  const url = chrome.runtime.getURL("options_ui/page.html");
+  chrome.windows.create({
+    url: url,
+    type: "popup",
+    width: 400,
+    height: 500,
+  });
 });
