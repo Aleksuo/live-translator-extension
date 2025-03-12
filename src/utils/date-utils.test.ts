4ea08d514ab0c63c881504bb3e1783c6dd20d7f2
Aleksi Suoranta <aleksi.suoranta@gmail.com>
2025-03-12 21:36:54 +0200

    Fix formatting

--- src/utils/date-utils.test.ts
+++ src/utils/date-utils.test.ts
@@ -1,16 +1,15 @@
 import { DateToHhMmSsString } from "./date-utils";
 
-describe('DateToHhMmSsString', () => {
+describe("DateToHhMmSsString", () => {
+  test("It should return a string of the Date in format HH:MM:SS", () => {
+    const mockDate = new Date("1999-12-13T14:20:50");
 
-    test('It should return a string of the Date in format HH:MM:SS', () => {
-        const mockDate = new Date('1999-12-13T14:20:50')
+    expect(DateToHhMmSsString(mockDate)).toBe("14:20:50");
+  });
 
-        expect(DateToHhMmSsString(mockDate)).toBe("14:20:50")
-    })
+  test("It should pad Hours, minutes and seconds", () => {
+    const mockDate = new Date("1999-12-13T04:02:05");
 
-    test('It should pad Hours, minutes and seconds', () => {
-        const mockDate = new Date('1999-12-13T04:02:05')
-
-        expect(DateToHhMmSsString(mockDate)).toBe("04:02:05")
-    })
-})
+    expect(DateToHhMmSsString(mockDate)).toBe("04:02:05");
+  });
+});
